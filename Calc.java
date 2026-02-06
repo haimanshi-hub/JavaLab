2cfaf88900ccd5d0cb3ab2e03be0abd024ccef35
haimanshi-hub <kya.yaar.saraa@gmail.com>
2026-02-05 23:53:02 -0800

    Program 1
    this program contains commands and arguments

---
+++ Calc.java
@@ -0,0 +1,24 @@
+public class Calc {
+        public static void main(String[] args){
+        int a=Integer.parseInt(args[0]);
+        int b=Integer.parseInt(args[1]);
+        add(a,b);
+        sub(a,b);
+        mul(a,b);
+        div(a,b);
+
+    }
+    public static void add(int a, int b){
+    System.out.println("Addition:"+(a+b));
+    }    
+   public static void sub(int a, int b){
+    System.out.println("Subtraction:"+(a-b));
+    }
+    public static void mul(int a, int b){
+    System.out.println("Multiplication:"+(a*b));
+    }
+    public static void div(int a, int b){
+    System.out.println("Division:"+(a/b));
+    }
+    
+}
